39d0aedb9724271864b5250d359550c7261d5e54
toby <t.hoelzl@students.htl-leonding.ac.at>
2025-03-16 10:35:08 +0100

    Design + functions days categorys

--- project/Plan/script/plan.js
+++ project/Plan/script/plan.js
@@ -1,22 +1,126 @@
 function addCategory(){
-    document.getElementById('categoryBox').innerHTML += `
-        <div class="category">
-            <p class="categorytext" onclick="rename(this.parentElement)">Name</p>
-        </div>
-    `;
+    for(i = 0; i < days.length; i++){
+        if (days[i].name == "Name"){
+            alert("gscheide Fehlermeldung einfügen");
+            return;
+        }
+    }
+
+    days.push(
+        {
+            "name": "Name",
+            "categorys": [
+                {
+                    "name": "Name",
+                    "sets": 0,
+                    "weight": 0,
+                    "reps": 0
+                }
+            ]
+        }
+    );
+
+    loaddays();
+
+
 }
 
 let categorytext;
+let text;
 function rename(e) {
-  e.innerHTML = `<input onchange="rename2(this)" class="inputNN">`;
-  categorytext = e;
+    event.stopPropagation();
+    text = e.innerHTML;
+    e.innerHTML = `<input onchange="rename2(this)" onblur="rename2(this)" class="inputNN">`;
+    categorytext = e;
   
-  const newInput = e.querySelector('.inputNN');
-  setTimeout(() => {
-    newInput.focus();
-  }, 0);
+    const newInput = e.querySelector('.inputNN');
+    setTimeout(() => {
+        newInput.focus();
+    }, 0);
 }
 
 function rename2(i){
-    categorytext.innerHTML = `<p class="categorytext" onclick="rename(this.parentElement)">${i.value}</p>`;
+    if (i.value == ""){
+        categorytext.innerHTML = `<p onclick="rename(this.parentElement)">${text}</p>`;
+    }else{
+        categorytext.innerHTML = `<p onclick="rename(this.parentElement)">${i.value}</p>`;
+    }
+    
 }
+
+function loaddays(){
+    document.getElementById('categoryBox').innerHTML = "";
+
+    for(i = 0; i < days.length; i++){
+        document.getElementById('categoryBox').innerHTML += `
+        <div class="category" onclick="loadcategorys(this)">
+            <p class="categorytext" onclick="rename(this.parentElement.querySelector('p'))">${days[i].name}</p>
+        </div>
+    `;
+    }
+}
+
+function loadcategorys(e){
+    document.getElementById('categoryBox').innerHTML = "";
+
+    for(i = 0; i < days.length; i++){
+        if (days[i].name == e.querySelector('p').innerHTML){
+            for(j = 0; j < days[i].categorys.length; j++){
+                document.getElementById('categoryBox').innerHTML += `
+                    
+
+
+
+
+            `;
+            }
+
+        }
+    }
+
+    
+}
+
+const days = [
+    {
+        "name": "Push",
+        "categorys": [
+            {
+                "name": "Chest",
+                exercises: [
+                    {
+                        "name": "Benchpress",
+                        "sets": 2,
+                        "weight": 90,
+                        "reps": 5
+                    },
+                    {
+                        "name": "Dips",
+                        "sets": 2,
+                        "weight": 90,
+                        "reps": 5
+                    }
+                ]
+            },
+            {
+                "name": "Shoulder",
+                exercises: [
+                    {
+                        "name": "Shoukderpress",
+                        "sets": 2,
+                        "weight": 90,
+                        "reps": 5
+                    },
+                    {
+                        "name": "Sidelift",
+                        "sets": 2,
+                        "weight": 90,
+                        "reps": 5
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+loaddays();
