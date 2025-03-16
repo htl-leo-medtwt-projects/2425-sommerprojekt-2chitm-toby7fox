cc0e9777c80ae4bd7b0930a4b26c79a89daa605c
toby <t.hoelzl@students.htl-leonding.ac.at>
2025-03-16 10:35:23 +0100

    optimized standart js and css

---
+++ project/standard/header_footer.js
@@ -0,0 +1,35 @@
+document.body.innerHTML += `
+    <div id="header">
+        <a href="../index.html">
+            <img src="../standard/img/home.png" alt="home" class="headerImg">
+        </a>
+        <div id="headerText">
+            <p id="headerTitel">Trainingsplan</p>
+            <p id="headerDate">today</p>
+        </div>
+        <img src="../standard/img/calender.png" alt="calender" class="headerImg">
+        <div id="userBox">
+            <img src="../standard/img/user.png" alt="user" id="userImg">
+        </div>
+    </div>
+    <div id="footer">
+        <a href="../Plan/plan.html">
+            <div class="footerBox">
+                <img src="../standard/img/plan.png" alt="plan" class="footerImg">
+                <p class="footerText">Plan</p>
+            </div>
+        </a>
+        <a href="../Entry/entry.html">
+            <div class="footerBox">
+                <img src="../standard/img/entry.png" alt="plan" class="footerImg">
+                <p class="footerText">Entry</p>
+            </div>
+        </a>
+        <a href="../Level/level.html">
+            <div class="footerBox">
+                <img src="../standard/img/level.png" alt="plan" class="footerImg">
+                <p class="footerText">Level</p>
+            </div>
+        </a>
+    </div>
+`;
