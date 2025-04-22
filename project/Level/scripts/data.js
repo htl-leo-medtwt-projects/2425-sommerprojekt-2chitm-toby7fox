8394a53d6624a0079ced6758e3801e0664925d38
toby <t.hoelzl@students.htl-leonding.ac.at>
2025-04-22 10:51:16 +0200

    erster Prototyp zum auslesen und in den chart einbauen von Daten

---
+++ project/Level/scripts/data.js
@@ -0,0 +1,95 @@
+var exerciseStats = {
+    "Benchpress": [
+        {
+            "year": 2025,
+            "months": [
+                {
+                    "month": 7,
+                    "weeks": [
+                        { "week": 1, "1RM": 106,  "date": "7.2025 Week 1" },
+                        { "week": 2, "1RM": 106.5, "date": "7.2025 Week 2" },
+                        { "week": 3, "1RM": 107,  "date": "7.2025 Week 3" },
+                        { "week": 4, "1RM": 107.5, "date": "7.2025 Week 4" }
+                    ]
+                },
+                {
+                    "month": 8,
+                    "weeks": [
+                        { "week": 1, "1RM": 108,  "date": "8.2025 Week 1" },
+                        { "week": 2, "1RM": 108.5, "date": "8.2025 Week 2" },
+                        { "week": 3, "1RM": 109,  "date": "8.2025 Week 3" },
+                        { "week": 4, "1RM": 109.5, "date": "8.2025 Week 4" }
+                    ]
+                },
+                {
+                    "month": 9,
+                    "weeks": [
+                        { "week": 1, "1RM": 110,  "date": "9.2025 Week 1" },
+                        { "week": 2, "1RM": 110.5, "date": "9.2025 Week 2" },
+                        { "week": 3, "1RM": 111,  "date": "9.2025 Week 3" },
+                        { "week": 4, "1RM": 111.5, "date": "9.2025 Week 4" }
+                    ]
+                },
+                {
+                    "month": 10,
+                    "weeks": [
+                        { "week": 1, "1RM": 112,  "date": "10.2025 Week 1" },
+                        { "week": 2, "1RM": 112.5, "date": "10.2025 Week 2" },
+                        { "week": 3, "1RM": 113,  "date": "10.2025 Week 3" },
+                        { "week": 4, "1RM": 113.5, "date": "10.2025 Week 4" }
+                    ]
+                },
+                {
+                    "month": 11,
+                    "weeks": [
+                        { "week": 1, "1RM": 114,  "date": "11.2025 Week 1" },
+                        { "week": 2, "1RM": 114.5, "date": "11.2025 Week 2" },
+                        { "week": 3, "1RM": 115,  "date": "11.2025 Week 3" },
+                        { "week": 4, "1RM": 115.5, "date": "11.2025 Week 4" }
+                    ]
+                },
+                {
+                    "month": 12,
+                    "weeks": [
+                        { "week": 1, "1RM": 116,  "date": "12.2025 Week 1" },
+                        { "week": 2, "1RM": 116.5, "date": "12.2025 Week 2" },
+                        { "week": 3, "1RM": 117,  "date": "12.2025 Week 3" },
+                        { "week": 4, "1RM": 117.5, "date": "12.2025 Week 4" }
+                    ]
+                }
+            ]
+        },
+        {
+            "year": 2026,
+            "months": [
+                {
+                    "month": 1,
+                    "weeks": [
+                        { "week": 1, "1RM": 118,  "date": "1.2026 Week 1" },
+                        { "week": 2, "1RM": 118.5, "date": "1.2026 Week 2" },
+                        { "week": 3, "1RM": 119,  "date": "1.2026 Week 3" },
+                        { "week": 4, "1RM": 119.5, "date": "1.2026 Week 4" }
+                    ]
+                },
+                {
+                    "month": 2,
+                    "weeks": [
+                        { "week": 1, "1RM": 120,  "date": "2.2026 Week 1" },
+                        { "week": 2, "1RM": 120.5, "date": "2.2026 Week 2" },
+                        { "week": 3, "1RM": 121,  "date": "2.2026 Week 3" },
+                        { "week": 4, "1RM": 121.5, "date": "2.2026 Week 4" }
+                    ]
+                },
+                {
+                    "month": 3,
+                    "weeks": [
+                        { "week": 1, "1RM": 122,  "date": "3.2026 Week 1" },
+                        { "week": 2, "1RM": 122.5, "date": "3.2026 Week 2" },
+                        { "week": 3, "1RM": 123,  "date": "3.2026 Week 3" },
+                        { "week": 4, "1RM": 123.5, "date": "3.2026 Week 4" }
+                    ]
+                }
+            ]
+        }
+    ]
+};
--- project/Level/scripts/level.js
+++ project/Level/scripts/level.js
@@ -1,37 +1,60 @@
 
+let exercise = "Benchpress";
+const labels = [];
+const data = [];
 
+for (let i = 0; i < exerciseStats.Benchpress.length; i++) {
+    for (let k = 0; k < exerciseStats.Benchpress[i].months.length; k++){
+        for (let u = 0; u < exerciseStats.Benchpress[i].months[k].weeks.length; u++){
+
+            labels.push(exerciseStats.Benchpress[i].months[k].weeks[u].date);
+            data.push(exerciseStats.Benchpress[i].months[k].weeks[u]["1RM"]);
+            
+        }
+    }
+}
+
+
+
+
+//Chart Erstellung---------------------------------------------------------
 const ctx = document.getElementById('myChart').getContext('2d');
-
-const labels = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni'];
-const data   = [100, 102, 106, 110, 120, 122];
-
-
 const canvas = document.getElementById('myChart');
 canvas.width = data.length * 120;
-
-
 const myChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: labels,
-      datasets: [{
-        label: 'Besucherzahlen',
-        data: data,
-        pointRadius: 10,
-      }]
+        labels: labels,
+        datasets: [{
+            label: { display: false },
+            data: data,
+            pointRadius: 10,
+        }]
     },
     options: {
-    responsive: false,
-      scales: {
-        y: {
-          ticks: {
-            stepSize: 1
-          },
-          grace: '10%'
+        responsive: false,
+        scales: {
+            y: {
+                ticks: {
+                    stepSize: 1
+                },
+                grace: '10%',
+                position: 'fixed',
+                left: '0'
+            },
         },
-      }
+        plugins: {
+            legend: { display: false },
+            tooltip: {
+                callbacks: {
+                    title: () => '',
+                    label: ctx => ctx.formattedValue
+                }
+            }
+        }
     }
 });
+//-----------------------------------------------------------------------
 
 const jetzt = new Date();
 console.log(jetzt.toLocaleDateString());
